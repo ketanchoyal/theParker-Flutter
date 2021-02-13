a1e61b94c5ab9b138b41b4e227ed78cfc0d4cd23
ketanchoyal <sharmaketanr@gmail.com>
2021-02-13 18:37:26 -0500

    ;-)

--- lib/UI/Resources/APIKeys.dart
+++
@@ -1,8 +0,0 @@
-class APIKeys {
-  static const google_map_key = 'AIzaSyCtVEL00yAITF2_5FUtdtSrQgR9L5uTyBo';
-  static const google_map_key2 = 'AIzaSyDIDCAbY9acmoT5c4ZbDFRlUBc-RHK2rTw';
-  // static const map_box_key2 =
-  //     "pk.eyJ1Ijoid293amFrZTEiLCJhIjoiMzMyMWY2Njk4MTA0NDZjNjVkMWVmNGQ3YWMzMjM3MTMifQ.MQ5_BYB1G5e6g0ne9AP0pg";
-  static const map_box_key =
-      'pk.eyJ1IjoicGFya2luZ3N5c3RlbSIsImEiOiJja2wwNW8xYzgwMWV1MnF0NG1taTlvZnpjIn0.g94N5mDC0tq2lO0Fw0fN9A';
-}
